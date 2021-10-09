7addbae55b4da4dc865cebef93a00044d4606fb5
Tanish013 <keshavdhingra0103@gmail.com>
2021-10-09 13:33:43 +0530

    Added Modles Views and Controllers

---
+++ BookWala/BookWala/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWala.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var obj = new { Id = 1, nameof = "Kd" };
+            return View(obj);
+        }
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookWala/BookWala/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookWala.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWala.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x=>x.Id==id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title,string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1,Title="MVC",Author="Kd"},
+                new BookModel(){Id=2,Title="MVC",Author="Kd"},
+                new BookModel(){Id=3,Title="C#",Author="Td"},
+                new BookModel(){Id=4,Title="Java",Author="Shu"},
+                new BookModel(){Id=5,Title="Cpp",Author="Shu"}
+            };
+        }
+    }
+}
